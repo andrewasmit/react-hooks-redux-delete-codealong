a4d43028d623ef4214b187357a65bf376eb5a3a4
Andrew <andrewasmit@hotmail.com>
2023-05-09 14:36:03 -0500

    done!

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -9,9 +9,13 @@ const todosSlice = createSlice({
     todoAdded(state, action) {
       state.entities.push(action.payload);
     },
+    todoRemoved(state, action){
+      const index = state.entities.findIndex(todo=>todo === action.payload);
+      state.entities.splice(index, 1);
+    }
   },
 });
 
-export const { todoAdded } = todosSlice.actions;
+export const { todoAdded, todoRemoved } = todosSlice.actions;
 
 export default todosSlice.reducer;
